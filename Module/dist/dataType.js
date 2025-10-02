7e90ed0d9e483f2f32f008af6cf18ecb6637afdb
Snu Seam <siamahme766@gmail.com>
2025-10-03 05:49:44 +0600

    comitting first module

---
+++ Module/dist/dataType.js
@@ -0,0 +1,15 @@
+"use strict";
+// Primitive data types 
+const num = 25;
+const myName = "sultan seam";
+const isDone = true;
+const undefine = undefined;
+const null1 = null;
+// non-primitive data types 
+// array type data 
+const friends = ['maidul', 'miqdad', 'mehedi'];
+friends[3] = 'ratan';
+// can't assign another type data in here 
+const rollNumber = [12, 34, 56, 35, 7, 5];
+rollNumber[3] = 43;
+console.log(rollNumber);
---
+++ Module/src/dataType.ts
@@ -0,0 +1,24 @@
+// Primitive data types 
+
+const num : number = 25;
+const myName : string = "sultan seam";
+const isDone : boolean = true;
+const undefine : undefined = undefined;
+const null1 : null = null;
+
+// non-primitive data types 
+
+// array type data 
+
+const friends : string[] = ['maidul', 'miqdad', 'mehedi'];
+friends[3] = 'ratan';
+// can't assign another type data in here 
+
+
+const rollNumber : number[] = [12,34,56,35,7,5] ;
+rollNumber[3]= 43;
+console.log(rollNumber);
+
+// topple type data 
+
+const nameRoll : [string, number,boolean] = ['nurul', 25, true];
