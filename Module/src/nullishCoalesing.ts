a4c70025c0d9c625ed0bcfb092093f7b7b8186fe
Snu Seam <siamahme766@gmail.com>
2025-10-04 18:40:18 +0600

    commit 1st module

---
+++ Module/src/nullishCoalesing.ts
@@ -0,0 +1,18 @@
+// optional chaining 
+// if a value is available or not then we use optional chaining 
+
+const age = 18;
+if (age >=18) {
+    console.log('adult');
+}else{
+    console.log('not adult');
+}
+
+const isAdult = age >=18 ? 'adult' : 'not adult';
+
+// nulish coalesing 
+
+const isAuthenticated = null;
+const result1 = isAuthenticated ?? 'Guest'; //here we will get guest when variable is null or undefined 
+// others time we will get empty string
+const result2 = isAuthenticated ? isAuthenticated : 'Guest'; //here we will get guest is there null 
